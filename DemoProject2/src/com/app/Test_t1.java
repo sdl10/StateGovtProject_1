eeb50f706a9e8fcbc0ed83d7f69dac04fd9d37e7
TL <nit.adilkhan0206@gmail.com>
2022-01-28 15:11:14 +0530

    commited by Dev

--- DemoProject2/src/com/app/Test_t1.java
+++ DemoProject2/src/com/app/Test_t1.java
@@ -2,9 +2,8 @@ package com.app;
 
 public class Test_t1 {
 
-	public static void main(String[] args) {
-
-
+	public static void main(String[] args) {	
+		System.out.println("Welcome to my home");
 	}
 
 }
