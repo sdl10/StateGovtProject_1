753db6ebd9e3d2dcf3c3567183cadab6c093ee0d
TL <nit.adilkhan0206@gmail.com>
2022-01-29 16:33:17 +0530

    added m2 method in Report package by TL

--- DemoProject2/src/com/app/report/Report.java
+++ DemoProject2/src/com/app/report/Report.java
@@ -1,9 +1,11 @@
 package com.app.report;
 
 public class Report {
-
+	public static void m2() {
+		System.out.println("Report Package");
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 
 	}
 
