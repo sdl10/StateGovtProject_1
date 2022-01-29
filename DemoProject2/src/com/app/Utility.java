882d6ed094dabdf413e92f67b4d556ee492728f9
TL <nit.adilkhan0206@gmail.com>
2022-01-29 15:58:20 +0530

    added sopln in m1 method by TL

--- DemoProject2/src/com/app/Utility.java
+++ DemoProject2/src/com/app/Utility.java
@@ -4,6 +4,7 @@ public class Utility {
 
 	public static void m1() {
 		System.out.println("Utility.m1()");
+		System.out.println("added by TL");
 	}
 	public static void main(String[] args) {
 		m1();
